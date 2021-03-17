5789d4d536de97b606ea016020129dc3ae799d3b
Wilsson A. Interiano <71723254+interale14@users.noreply.github.com>
2021-03-16 19:17:54 -0600

    Covid Page
    Asignación de la página covid como inicio

--- src/App.js
+++ src/App.js
@@ -1,15 +1,7 @@
 import './App.css';
 import BrandHeader from './components/BrandHeader/BrandHeader';
-import Login from './components/Pages/Login';
-import Home from './components/Pages/Home';
-import Todo from './components/Pages/Todo/Todo';
-import Covid from './components/Pages/Covid/Covid';
+import Covid from './components/Covid/Covid';
 import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
-//import Cards from './components/Cards/Cards';
-//import CardHolder from './components/Cards/CardHolder';
-//
-
-//import imgMeme from './assets/meme.jpg';
 
 function App() {
   return (
@@ -18,16 +10,7 @@ function App() {
         <BrandHeader></BrandHeader>
         <Switch>
           <Route path="/" exact>
-              <Home />
-          </Route>
-          <Route path="/todos" exact>
-            <Todo />
-          </Route>
-          <Route path="/login" exact>
-              <Login></Login>
-          </Route>
-          <Route path="/covid" exact>
-            <Covid></Covid>
+              <Covid />
           </Route>
         </Switch>
       </section>
@@ -36,17 +19,3 @@ function App() {
 }
 
 export default App;
-
-/*
-
-<section>
-        <img src={imgMeme} alt="Un meme" />
-      </section>
-      <CardHolder>
-        <Cards></Cards>
-        <Cards></Cards>
-        <Cards></Cards>
-        <Cards></Cards>
-      </CardHolder>
-
-*/
