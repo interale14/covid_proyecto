a43fa332cf4233ae2401b0b103f1c5b41788f384
Wilsson A. Interiano <71723254+interale14@users.noreply.github.com>
2021-03-17 21:16:49 -0600

    Interfaz
    Diseño de la interfaz

--- src/components/Forms/FormField.js
+++ src/components/Forms/FormField.js
@@ -1,16 +1,15 @@
 function FormField({ type, id, fname, onChange, caption, value, error, labelClassName, containerClassName, ...attr}){
   return (
-    <section className={containerClassName}>
-    <label className={labelClassName}>{caption}</label>
-    <input
-        type={type}
-        id={id}
-        name={fname}
-        onChange={onChange}
-        value={value}
-        {...attr}
-    />
-      {(error && true) ? (<section>{error}</section>):null }
+    <section className="w-10/12 border-2 border-gray-500 border-solid">
+      <input
+          type={type}
+          id={id}
+          name={fname}
+          onChange={onChange}
+          value={value}
+          {...attr}
+      />
+        {(error && true) ? (<section>{error}</section>):null }
     </section>
   )
 }
