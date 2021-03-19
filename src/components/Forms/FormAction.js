c7b18c5b0d75d2609d7479b86d6be5262551ac38
Wilsson A. Interiano <71723254+interale14@users.noreply.github.com>
2021-03-19 14:19:49 -0600

    Revert "Interfaz"
    This reverts commit a43fa332cf4233ae2401b0b103f1c5b41788f384.

--- src/components/Forms/FormAction.js
+++ src/components/Forms/FormAction.js
@@ -1,9 +1,29 @@
 function FormAction({caption, onClick}){
-  
+  let myClasses = [
+    "w-full",
+    "flex",
+    "items-center",
+    "justify-center",
+    "px-8",
+    "py-4",
+    "my-4",
+    "border",
+    "border-transparent",
+    "text-base",
+    "font-medium",
+    "rounded-lg",
+    "text-white",
+    "text-indigo-700",
+    "bg-indigo-100",
+    "hover:bg-indigo-200",
+    "md:py-4",
+    "md:text-lg",
+    "md:px-10"
+  ];
   return(
     <button
       onClick={onClick}
-      className="flex items-center justify-center px-3 py-0.5 bg-indigo-100 lg:w-1/12 w-3/12"
+      className={myClasses.join(" ")}
     >
       {caption}
     </button>
